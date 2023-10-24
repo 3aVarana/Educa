ba1c177dabb942afb51ef3dcff6716f657533ce4
Andres Arana <3av.arana@gmail.com>
2023-10-24 17:02:48 -0400

    Add background

--- Educa/views/CoursesView.swift
+++ Educa/views/CoursesView.swift
@@ -9,23 +9,32 @@ import SwiftUI
 
 struct CoursesView: View {
     @ObservedObject var viewModel = CoursesViewModel()
-    let background = LinearGradient(colors: [.red, .red.opacity(0.8), .yellow.opacity(0.8)], startPoint: .top, endPoint: .bottom)
     
     var body: some View {
         NavigationView {
             List(viewModel.courses) { course in
                 ZStack(alignment: .leading) {
                     NavigationLink(destination: CourseDetail(course: course)) { }
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(.linearGradient(colors: [.green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .shadow(color: .black, radius: 8, x: 4, y: 4)
-                    VStack(alignment: .leading, spacing: 16) {
-                        Text(course.name)
-                            .font(.system(.title2, design: .monospaced))
-                        Text(course.code)
-                            .font(.system(.title3))
-                        Text("Par. \(course.parallel)")
-                            .font(.system(.body))
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.gray)
+                            .shadow(color: .black, radius: 4, x: 4, y: 4)
+                        RoundedRectangle(cornerRadius: 8)
+                            .strokeBorder(.linearGradient(colors: [.black, .white], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    }
+                    HStack {
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text(course.name)
+                                .font(.system(.title2, design: .monospaced))
+                            Text(course.code)
+                                .font(.system(.title3))
+                            Text("Par. \(course.parallel)")
+                                .font(.system(.body))
+                        }
+                        Image(systemName: "house")
+                            .resizable()
+                            .frame(height: 64)
+                            .scaledToFill()
                     }
                     .padding()
                 }
@@ -36,9 +45,7 @@ struct CoursesView: View {
             }
             .listStyle(PlainListStyle())
             .navigationBarTitle("Courses")
-            .background(background.ignoresSafeArea())
             .scrollContentBackground(.hidden)
-            .navigationBarBackground(.red)
         }
     }
 }
@@ -47,24 +54,4 @@ struct CoursesView_Previews: PreviewProvider {
     static var previews: some View {
         CoursesView()
     }
-}
-
-extension View {
-  func navigationBarBackground(_ background: Color = .orange) -> some View {
-    return self
-      .modifier(ColoredNavigationBar(background: background))
-  }
-}
-
-struct ColoredNavigationBar: ViewModifier {
-  var background: Color
-  
-  func body(content: Content) -> some View {
-    content
-      .toolbarBackground(
-        background,
-        for: .navigationBar
-      )
-      .toolbarBackground(.visible, for: .navigationBar)
-  }
 }
