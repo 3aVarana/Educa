4a4b051379e1e7f12aac75b72edc80eecf553aa0
Andres Arana <3av.arana@gmail.com>
2023-10-25 22:18:25 -0400

    Create basic view models

---
+++ Educa/view models/CalendarViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarViewModel.swift
+//  Educa
+//
+//  Created by Victor Arana on 10/25/23.
+//
+
+import Foundation
+
+class CalendarViewModel: ObservableObject {
+    
+}
---
+++ Educa/view models/StudentListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  StudentListViewModel.swift
+//  Educa
+//
+//  Created by Victor Arana on 10/25/23.
+//
+
+import Foundation
+
+class StudentListViewModel: ObservableObject {
+    
+}
